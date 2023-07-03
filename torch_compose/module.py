479d4898bab11f742eeba404ee85688fe493c5ca
Alex Naka <alex.naka@gmail.com>
2023-07-03 16:57:56 -0700

    refactoring

--- torch_compose/module.py
+++ torch_compose/module.py
@@ -17,6 +17,8 @@ class DirectedModule(nn.Module):
         **kwargs,
     ):
         super().__init__()
+
+        
         if isinstance(input_keys, str):
             input_keys = [input_keys]
         try:
@@ -34,49 +36,40 @@ class DirectedModule(nn.Module):
             output_keys = tuple(output_keys) # if output_keys is not a dict, assume it is a collection and convert to tuple
         self.output_keys = output_keys
         
-    def _unpack_inputs(self, batch:Dict) -> Dict[str, torch.Tensor]:
-        if isinstance(self.input_keys, Tuple):
-            return 
-        return 
-    
-    def _update_batch_dict(self, batch:Dict, output:Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        for batch_key, internal_key in self.output_key_remapping.items():
-            batch[batch_key] = output[internal_key]
+
+    def _graph_forward(self, batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        outputs = self._get_forward_outputs(batch)
+
+        if isinstance(self.output_keys, dict):
+            self._process_dict_outputs(batch, outputs)
+        elif isinstance(self.output_keys, tuple):
+            self._process_tuple_outputs(batch, outputs)
+
         return batch
 
-    def _graph_forward(self, batch:Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-
-        outputs = {}
+    def _get_forward_outputs(self, batch):
         if isinstance(self.input_keys, Dict):
-            # if input_keys is dict, assume we want to remap keys before passing to forward
             inputs = {internal_key: batch.get(batch_key, None) for batch_key, internal_key in self.input_keys.items()}
-            # pass named arguments to forward
-            outputs = self.forward(**inputs)
-        elif isinstance(self.input_keys, Tuple):
-            # if input_keys is tuple, assume no remapping is needed and pass positional arguments to forward
+            return self.forward(**inputs)
+        else:  # self.input_keys is a Tuple
             inputs = [batch.get(batch_key, None) for batch_key in self.input_keys]
-            outputs = self.forward(*inputs)
-            
-        if isinstance(self.output_keys, Dict):
-            assert isinstance(outputs, Dict) # if output_keys is dict, assume we want to remap keys before adding to batch
-            for internal_key, batch_key in self.output_keys.items():
+            return self.forward(*inputs)
+
+    def _process_dict_outputs(self, batch, outputs):
+        assert isinstance(outputs, Dict)  # if output_keys is dict, assume we want to remap keys before adding to batch
+        for internal_key, batch_key in self.output_keys.items():
+            batch[batch_key] = outputs[internal_key]
+
+    def _process_tuple_outputs(self, batch, outputs):
+        if isinstance(outputs, Dict):
+            for internal_key, batch_key in zip(self.output_keys, outputs.keys()):
                 batch[batch_key] = outputs[internal_key]
-            return batch
-        elif isinstance(self.output_keys, Tuple): # if output_keys is tuple, assume no remapping is needed
-            if isinstance(outputs, Dict):
-                for internal_key, batch_key in zip(self.output_keys, outputs.keys()):
-                    batch[batch_key] = outputs[internal_key]
-            elif isinstance(outputs, Tuple):
-                for batch_key, output in zip(self.output_keys, outputs):
-                    batch[batch_key] = output
-            elif isinstance(outputs, torch.Tensor):
-                for batch_key in self.output_keys:
-                    batch[batch_key] = outputs
-            return batch
-        
-        batch = self._update_batch_dict(batch, outputs)
-        
-        return batch
+        elif isinstance(outputs, Tuple):
+            for batch_key, output in zip(self.output_keys, outputs):
+                batch[batch_key] = output
+        elif isinstance(outputs, torch.Tensor):
+            for batch_key in self.output_keys:
+                batch[batch_key] = outputs
     
     @property
     def _input_batch_keys(self):
@@ -98,10 +91,24 @@ class DirectedModule(nn.Module):
 
 
 class ModuleGraph(nn.ModuleDict):
-    def __init__(self, modules: Dict[str, 'DirectedModule']=None):
+    def __init__(self, modules: Dict[str, 'DirectedModule'] = None):
         modules = modules or {}
-        sorted_modules, self.module_graph = self._sort_submodules(modules)
+        sorted_modules, graph = self._build_and_sort_graph(modules)
         super().__init__(modules=sorted_modules)
+        self.module_graph = graph
+
+    def _build_and_sort_graph(self, modules):
+        graph = self._build_graph(modules)
+        sorted_keys = self._sort_keys(graph)
+        sorted_modules = self._create_sorted_modules(sorted_keys, modules)
+        return sorted_modules, graph
+
+    def _create_sorted_modules(self, sorted_keys, modules):
+        sorted_modules = OrderedDict()
+        for key in sorted_keys:
+            if key in modules:
+                sorted_modules[key] = modules[key]
+        return sorted_modules
         
     def _build_graph(self, modules):
         graph = nx.DiGraph()
@@ -132,16 +139,6 @@ class ModuleGraph(nn.ModuleDict):
         graph_dict = {node: set(graph.predecessors(node)) for node in graph.nodes()}
         return list(TopologicalSorter(graph_dict).static_order())
 
-    def _sort_submodules(self, modules):
-        graph = self._build_graph(modules)
-        sorted_keys = self._sort_keys(graph)
-        
-        sorted_modules = OrderedDict()
-        for key in sorted_keys:
-            if key in modules:
-                sorted_modules[key] = modules[key]
-        
-        return sorted_modules, graph
 
     def forward(self, batch: dict):
         for module_name, module in self.items():
